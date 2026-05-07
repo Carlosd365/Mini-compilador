bd5df640b53a516e973126962cfdfae196b64fbb
Carlosd365 <carlitosgoag3@gmail.com>
2026-05-07 13:23:02 -0600

    Shadowing

--- mini_compilador.py
+++ mini_compilador.py
@@ -33,6 +33,23 @@ print(json.dumps(ast.a_dict(), indent=2, ensure_ascii=False))
 print("\nSEMANTICO")
 sem = AnalizadorSemantico()
 
+print("\nShadowing")
+print("""2. Detección de Errores Semanticos
+
+
+Error 1: Y es int pero y+x produce un float
+Error 2: Se reasigna x sin tipo declarado 
+Error 3: Z nunca fue definida
+
+
+3. Fenómeno del Shadowing
+
+¿Cómo decide su método obtener_tipo_variable cuál utilizar?
+Al encontrar un Identificador, esta revisa la lista de ámbitos de manera invertida, si el nombre del identificador se encuentra en la lista de ámbitos de donde se encuentra, retorna el ámbito para ser analizado, si esta no es retornada, todo funciona correctamente
+
+¿Qué valor (o tipo) se usaría para x en la línea y = y + x;?
+La segunda, el float según shadowing ya que al estar en un ámbito interno y diferente {} al externo, no va tocar al externo mientras no salga.""")
+
 try:
     sem.analizar(ast)
 except Exception as e:
